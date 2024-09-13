249af1329c015ed57540e63987e89a641377076e
vimal1819 <vimalamrish@gmail.com>
2024-09-13 09:38:51 +0530

    my first commit

---
+++ src/test/java/Day1/Git.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class Git {
+	public void method1() {
+		System.out.println("amrish");
+	}
+	public void method2(){
+		System.out.println("chennai");
+	}
+	public void method3() {
+		System.out.println("method3");
+	}
+
+}
